6fea43c00f27cd2b2738579f4119336cd36e68ff
dineshri <dineshripillay@yahoo.com>
2016-03-29 20:51:52 +0200

    static varible included

--- tut5- Employee/Source.cpp
+++ tut5- Employee/Source.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-
-
 class Employee {
 protected:
 	string m_name;
 	int Number;
 
 public:
-    Employee(string em_name, int num = 1): m_name(em_name), Number(num){  } // constructor of base class
+    Employee(string em_name, int num): m_name(em_name), Number(num){  } // constructor of base class
 	
+	static int numberOfEmployees;
+
 	string name() {
 		return m_name;}   // method name()
 
@@ -21,15 +21,15 @@ public:
 
 	virtual float salary() = 0; // pure virtual function for salary to be modified in derived classes
 
-	
+	};
 
-};
+int Employee::numberOfEmployees = 0;
 
 // derived classes starts here
 
 class SalaryEmployee : public Employee {
 public:
-	SalaryEmployee(string em_name, int num = 1) : Employee(em_name, num = 1){ } // default constructor
+	SalaryEmployee(string em_name, int num) : Employee(em_name, num){ } // default constructor
 	float sal;
 
 	const void setSalary(float m_salary) { // declared constant for a fixed salary
@@ -40,13 +40,15 @@ public:
 		return sal; // salary calculation
 	}
 
-	
+	int SalaryEmployee::numberOfEmployees = Employee::numberOfEmployees++;
 
-};
+    };
+
+
 
 class HourlyEmployee : public Employee {
 public:
-	HourlyEmployee(string em_name, int num = 1) : Employee(em_name, num = 1){ } // default constructor
+	HourlyEmployee(string em_name, int num = 1) : Employee(em_name, num){ } // default constructor
 	float hourly_rate;
 	int hours_worked;
 
@@ -62,12 +64,15 @@ public:
 		return (hourly_rate * hours_worked); // salary calculation 
 	}
 
-	
-};
+	int HourlyEmployee::numberOfEmployees = Employee::numberOfEmployees++;
+
+	};
+
+
 
 class CommissionEmployee : public Employee {
 public:
-	CommissionEmployee(string em_name, int num = 1) : Employee(em_name, num = 1){ } // default constructor
+	CommissionEmployee(string em_name, int num) : Employee(em_name, num){ } // default constructor
 	float base_salary;
 	float rate;
 	float revenue;
@@ -88,18 +93,20 @@ public:
 		return (base_salary + rate + revenue); // salary calculation (not sure if correct)
 	}
 
-	
-};
+	int CommissionEmployee::numberOfEmployees = Employee::numberOfEmployees++;
+
+	};
+
+
 
 
 // testing 
 
 int main() {
 	SalaryEmployee Em1 ("Din", 1);
-	HourlyEmployee Em2 ("Povz", 2);
+	HourlyEmployee Em2 ("Povz", 5);
 	CommissionEmployee Em3 ("Dee", 3);
 	
-
 	Em1.setSalary(4000);
 
 	Em2.setHourlyRate(1000);
@@ -109,9 +116,10 @@ int main() {
 	Em3.setRate(500);
 	Em3.setRevenue(2500);
 
-	cout << Em1.name() << " earns " << Em1.salary() << endl;
-	cout << Em2.name() << " earns " << Em2.salary() << endl;
-	cout << Em3.name() << " earns " << Em3.salary() << endl;
-
+	cout << Em1.name() << " with staff number " << Em1.staffNumber() << " earns " << Em1.salary() << endl;
+	cout << Em2.name() << " with staff number " << Em2.staffNumber() << " earns " << Em2.salary() << endl;
+	cout << Em3.name() << " with staff number " << Em3.staffNumber() << " earns " << Em3.salary() << endl;
+	cout << "Total number of employees: "<< Employee::numberOfEmployees << endl;
 	
+	system("pause");
 }
